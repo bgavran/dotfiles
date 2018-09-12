8838bcc7bcee5a1965c36379384d0dbb3061abfa
Bruno Gavranović <bruno.gavranovic@fer.hr>
2018-09-13 00:50:51 +0200

    misc changes

--- .config/polybar/cpuinfo.sh
+++ .config/polybar/cpuinfo.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 #SPEED=$(sensors | grep fan2 | grep -o "[0-9]\{2,4\}")
-FREQ=$(cat /proc/cpuinfo | grep MHz | head -n1 | awk -F':' '{print $2}' | awk -F'.' '{print $1}')
-TEMP=$(sensors | grep "Package id 0" | awk '{print $4}' | sed 's/+//g')
+FREQ=$(cat /proc/cpuinfo | grep MHz | head -n1 | awk -F':' '{print $2/1000}' | cut -c -3)
+
+#TEMP=$(sensors | grep "Package id 0" | awk '{print $4}' | sed 's/+//g' | sed 's/^\(.\{2\}\)./\1/' | sed 's/^\(.\{2\}\)./\1/')
 # echo ${FREQ}MHz ${SPEED}RPM ${TEMP}
-echo ${FREQ}MHz ${TEMP}
+echo ${FREQ}GHz
